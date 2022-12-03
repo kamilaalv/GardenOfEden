80f2419f51a24fef4d664ae3a418a6f5d70669a5
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2022-12-03 12:58:31 +0300

    Bouquet

--- GardenOfEden/src/Bouquet.java
+++ GardenOfEden/src/Bouquet.java
@@ -2,12 +2,35 @@ import java.util.ArrayList;
 
 public class Bouquet {
 
+	public enum WRAPPINGPAPER  //enum for the color
+	{
+		RED("RED"),
+		BLUE("BLUE"),
+		PURPLE("PURPLE"),
+		GREEN("GREEN");
+		
+		private final String wrapcolor;
+		
+		private WRAPPINGPAPER(String wrapcolor)
+		{
+			this.wrapcolor=wrapcolor;
+		}
+	}
+	WRAPPINGPAPER wrappaper=null;
+
+	
+	private final double red=3f;
+	private final double blue=4f;
+	private final double green=5f;
+	private final double purple=6f;
+
+	
 	private ArrayList<Flower> flowerQuantity= new ArrayList<>();
 	private int maxQuantity;
 	private String card;
 	private String wrappingPaper;
 	private ArrayList<String> Addons= new ArrayList<String>();
-	
+	private double BouquetPrice;
 	public Bouquet() {
 		super();
 	}
@@ -20,6 +43,14 @@ public class Bouquet {
 		this.card = card;
 		this.wrappingPaper = wrappingPaper;
 		Addons = addons;
+	}
+
+	public double getBouquetPrice() {
+		return BouquetPrice;
+	}
+
+	public void setBouquetPrice(double bouquetPrice) {
+		BouquetPrice = bouquetPrice;
 	}
 
 	public ArrayList<Flower> getFlowerQuantity() {
@@ -63,7 +94,47 @@ public class Bouquet {
 	}
 	
 	public double calculateBouquetPrice() {
-		return maxQuantity;
+		
+		double TotalPrice=0f;
+	
+		
+		
+		switch(wrappingPaper)
+		{
+			case "RED":
+				wrappaper=wrappaper.RED;
+				setBouquetPrice(getBouquetPrice() + red );
+				break;
+				
+			case "GREEN":
+				wrappaper=wrappaper.GREEN;
+				setBouquetPrice(getBouquetPrice() + green );
+				break;
+				
+			case "BLUE":
+				wrappaper=wrappaper.BLUE;
+				setBouquetPrice(getBouquetPrice() + blue );
+				break;
+			
+			case "PURPLE":
+				wrappaper=wrappaper.PURPLE;
+				setBouquetPrice(getBouquetPrice() + purple );
+				break;
+				
+		}
+		
+		if(flowerQuantity.size()==0)
+		{
+			return 0;
+		}
+		
+		for(int i=0; i<flowerQuantity.size(); i++)
+		{
+			TotalPrice+= flowerQuantity.get(i).getItemPrice();
+		}
+		
+		setBouquetPrice(TotalPrice);
+		return getBouquetPrice();
 		
 	}
 	
