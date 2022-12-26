46a67d3eef87e1cd71fe6c01c29ba1b067844517
kamilaalv <kamilaaliyeva58@gmail.com>
2022-12-26 18:25:40 +0300

    deleting everything

--- GardenOfEden/src/InheritanceClasses/ItemInterface.java
+++
@@ -1,9 +0,0 @@
-package InheritanceClasses;
-import java.util.Date;
-
-public interface ItemInterface {
-	
-	public abstract boolean EligibleForDiscount();
-	public abstract void GenerateID();  
-
-}
